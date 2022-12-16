0d301fe3932b48156ab7454c28f5d8d0dcbee09a
unknown <taherallaa>
2022-12-16 02:59:03 +0200

    last update

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+import javax.swing.JOptionPane;
+import java.sql.*;
+
+public class Main {
+	
+	public static Connection connectMySql() {
+		try {
+				
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+		return null;
+	}
+
+
+	public static void main(String[] args) {
+	}
+	
+}
